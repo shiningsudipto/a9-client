f3f5903d4b056a4c59d48ef10fc32d00225d238d
Sudipta Das <shiningsudipto@gmail.com>
2024-12-15 22:26:34 +0600

    wrap with persistor

--- src/main.tsx
+++ src/main.tsx
@@ -4,17 +4,20 @@ import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import router from "./routes/router";
 import { Provider } from "react-redux";
-import { store } from "./redux/store";
+import { persistor, store } from "./redux/store";
 import { ThemeProvider } from "@material-tailwind/react";
 import { Toaster } from "sonner";
+import { PersistGate } from "redux-persist/integration/react";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Provider store={store}>
-      <ThemeProvider>
-        <RouterProvider router={router} />
-        <Toaster />
-      </ThemeProvider>
+      <PersistGate loading={null} persistor={persistor}>
+        <ThemeProvider>
+          <RouterProvider router={router} />
+          <Toaster />
+        </ThemeProvider>
+      </PersistGate>
     </Provider>
   </StrictMode>
 );
